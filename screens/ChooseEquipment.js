f966c61b87e72071e0cd930a488b61f21215b562
Benjamin Landry <benlandry12418@gmail.com>
2024-06-21 16:12:18 -0400

    Added touchables for each equipment, checkbox needs fixing

--- screens/ChooseEquipment.js
+++ screens/ChooseEquipment.js
@@ -1,8 +1,9 @@
 import { CurrentRenderContext, NavigationContainer } from '@react-navigation/native';
 import React, { useState } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { View, Text, Dimensions, Button, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, Image, Dimensions, Button, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import CheckBox from 'expo-checkbox'
 
 const { width, height } = Dimensions.get('window');
 
@@ -10,17 +11,132 @@ const Stack = createStackNavigator();
 
 const ChooseEquipment = () => {
   const [text, setText] = useState('');
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handlePress = () => {
+    setIsChecked(!isChecked);
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-        <View style={styles.inputContainer}>
-            <Text style={styles.label}>Choose equipment:</Text>
-            <TextInput
-            style={styles.input}
-            placeholder="Search"
-            onChangeText={setText} // Updates the state variable 'text' whenever the input changes
-            value={text} // Binds the input value to the state variable 'text'
+        <View style={styles.equipmentContainer}>
+          <TouchableOpacity 
+            style={styles.touchable}
+            onPress={handlePress}
+            >
+            <Image
+                source={require('../assets/equipment_icons/dumbbell-icon.png')}
+                style={styles.icons}
             />
+            <Text style={styles.equipmentText}>Dumbbells</Text>
+            <CheckBox 
+              value={isChecked}
+              onValueChange={handlePress}
+              style={styles.checkbox}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.equipmentContainer}>
+          <TouchableOpacity 
+            style={styles.touchable}
+            onPress={handlePress}
+            >
+            <Image
+                source={require('../assets/equipment_icons/barbell-icon.png')}
+                style={styles.icons}
+            />
+            <Text style={styles.equipmentText}>Barbells</Text>
+            <CheckBox 
+              value={isChecked}
+              onValueChange={handlePress}
+              style={styles.checkbox}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.equipmentContainer}>
+          <TouchableOpacity 
+            style={styles.touchable}
+            onPress={handlePress}
+            >
+            <Image
+                source={require('../assets/equipment_icons/kettlebell-icon.png')}
+                style={styles.icons}
+            />
+            <Text style={styles.equipmentText}>Kettlebell</Text>
+            <CheckBox 
+              value={isChecked}
+              onValueChange={handlePress}
+              style={styles.checkbox}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.equipmentContainer}>
+          <TouchableOpacity 
+            style={styles.touchable}
+            onPress={handlePress}
+            >
+            <Image
+                source={require('../assets/equipment_icons/cable-icon.png')}
+                style={styles.icons}
+            />
+            <Text style={styles.equipmentText}>Cables</Text>
+            <CheckBox 
+              value={isChecked}
+              onValueChange={handlePress}
+              style={styles.checkbox}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.equipmentContainer}>
+          <TouchableOpacity 
+            style={styles.touchable}
+            onPress={handlePress}
+            >
+            <Image
+                source={require('../assets/equipment_icons/machine-icon.png')}
+                style={styles.icons}
+            />
+            <Text style={styles.equipmentText}>Machines</Text>
+            <CheckBox 
+              value={isChecked}
+              onValueChange={handlePress}
+              style={styles.checkbox}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.equipmentContainer}>
+          <TouchableOpacity 
+            style={styles.touchable}
+            onPress={handlePress}
+            >
+            <Image
+                source={require('../assets/equipment_icons/bodyweight-icon.png')}
+                style={styles.icons}
+            />
+            <Text style={styles.equipmentText}>Bodyweight</Text>
+            <CheckBox 
+              value={isChecked}
+              onValueChange={handlePress}
+              style={styles.checkbox}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.equipmentContainer}>
+          <TouchableOpacity 
+            style={styles.touchable}
+            onPress={handlePress}
+            >
+            <Image
+                source={require('../assets/equipment_icons/cardio-icon.png')}
+                style={styles.icons}
+            />
+            <Text style={styles.equipmentText}>Cardio</Text>
+            <CheckBox 
+              value={isChecked}
+              onValueChange={handlePress}
+              style={styles.checkbox}
+            />
+          </TouchableOpacity>
         </View>
     </SafeAreaView>
   );
@@ -49,6 +165,32 @@ const styles = StyleSheet.create({
   },
   displayText: {
     fontSize: 18, // Sets the font size for the display text
+  },
+  equipmentContainer:{
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    marginTop: 20,
+    justifyContent: 'space-between'
+  },
+  icons: {
+    marginLeft: 10,
+    height: 40,
+    width: 40,
+  },
+  checkbox: {
+    marginRight: 20,
+  },
+  equipmentText: {
+    fontSize: 18,
+  },
+  touchable: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent:'space-between',
+    padding: 10,
+    backgroundColor: '#ddd',
+    borderRadius: 10,
   },
 });
 
