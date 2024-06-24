15525e3faab3366091757c386628cfbaf99f80fe
Benjamin Landry <benlandry12418@gmail.com>
2024-06-23 21:29:47 -0400

    Adding data for each exercise for filtering

---
+++ screens/ExerciseData.js
@@ -0,0 +1,45 @@
+// exerciseData.js
+export const exercises = [
+    {
+      id: 1,
+      title: "Barbell Bench Press",
+      image: require('../assets/exercise_icon_gifs/barbell_bench_press.gif'),
+      muscle: "Chest",
+      equipment: "Barbell",
+    },
+    {
+      id: 2,
+      title: "Dumbbell Bench Press",
+      image: require('../assets/exercise_icon_gifs/dumbbell_bench_press.gif'),
+      muscle: "Chest",
+      equipment: "Dumbbell",
+    },
+    {
+      id: 3,
+      title: "Dumbbell One-Arm Row",
+      image: require('../assets/exercise_icon_gifs/dumbbell_one_arm_row.gif'),
+      muscle: "Middle Back",
+      equipment: "Dumbbell",
+    },
+    {
+      id: 4,
+      title: "Dumbbell Row",
+      image: require('../assets/exercise_icon_gifs/dumbbell_row.gif'),
+      muscle: "Middle Back",
+      equipment: "Dumbbell",
+    },
+    {
+      id: 5,
+      title: "Decline Cable Crossover",
+      image: require('../assets/exercise_icon_gifs/decline_cable_crossover.gif'),
+      muscle: "Chest",
+      equipment: "Cable",
+    },
+    {
+      id: 6,
+      title: "Incline Dumbbell Bench Press",
+      image: require('../assets/exercise_icon_gifs/incline_dumbbell_press.gif'),
+      muscle: "Chest",
+      equipment: "Dumbbell",
+    },
+  ];
